6d50fce276ecfef6a9f22eb39320b2cfb935f592
Américo Cantillo <americo@americos-macbook-pro.local>
2016-11-09 18:55:36 -0500

    Clase para IBOutlets del control de PresupuestosCell

---
+++ PresupuestoCell.swift
@@ -0,0 +1,44 @@
+//
+//  PresupuestoCell.swift
+//  My Finance Controller
+//
+//  Created by Américo Cantillo on 9/11/16.
+//  Copyright © 2016 Américo Cantillo Gutiérrez. All rights reserved.
+//
+
+import UIKit
+
+class PresupuestoCell: UITableViewCell {
+    
+    @IBOutlet weak var titulo: UILabel!
+    
+    @IBOutlet weak var rangoFechas: UILabel!
+
+    @IBOutlet weak var porcentaje: UILabel!
+    
+    @IBOutlet weak var valorPresupuesto: UILabel!
+    
+    @IBOutlet weak var valorIngresos: UILabel!
+    
+    @IBOutlet weak var valorEgresos: UILabel!
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String!) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
